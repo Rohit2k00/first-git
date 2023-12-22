c360e6e35b3d0a1bccc64239080098be778f8f37
ninja <rwasnik1112@gmail.com>
2023-12-22 14:47:22 +0530

    this is updated main google file

--- google.java
+++ google.java
@@ -37,3 +37,37 @@ echo "Product: $product"
 if [ -n "$division" ]; then
     echo "Division: $division"
 fi
+#!/bin/bash
+
+# Check if two arguments are provided
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <number1> <number2>"
+  exit 1
+fi
+
+# Check if the arguments are valid numbers
+if ! [[ $1 =~ ^[0-9]+(\.[0-9]+)?$ ]] || ! [[ $2 =~ ^[0-9]+(\.[0-9]+)?$ ]]; then
+  echo "Error: Please provide valid numeric arguments."
+  exit 1
+fi
+
+# Assign the command-line arguments to variables
+num1="$1"
+num2="$2"
+
+# Perform calculations
+sum=$(bc <<< "$num1 + $num2")
+difference=$(bc <<< "$num1 - $num2")
+product=$(bc <<< "$num1 * $num2")
+
+if [ "$(bc <<< "$num2 != 0")" -eq 1 ]; then
+  division=$(bc <<< "$num1 / $num2")
+else
+  division="Error: Division by zero is not allowed."
+fi
+
+# Display the results
+echo "Sum: $sum"
+echo "Difference: $difference"
+echo "Product: $product"
+echo "Division: $division"
